ab6e0aacde8d0ddf978d78be3e69b7c89eb2f7ac
kerem.ea <krembertaktas@gmail.com>
2025-03-20 11:33:44 +0100

    Starten af vektor biblioteket

---
+++ source/index.js
@@ -0,0 +1,32 @@
+class Vector {
+	constructor(x_pos, y_pos, x_comp, y_comp, length, thickness, color, name) {
+		this.x = x_pos;
+		this.y = y_pos;
+		this.x_comp = x_comp;
+		this.y_comp = y_comp;
+		this.length = length;
+		this.thickness = thickness;
+		this.color = color;
+		this.name = name;
+	}
+
+	show() {
+		line()
+	}
+
+
+
+}
+
+
+
+
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw()
+{
+
+}
