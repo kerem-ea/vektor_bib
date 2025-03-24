377fac9a9f4a203ba652bf5722f9e4fe647c2577
kerem.ea <krembertaktas@gmail.com>
2025-03-24 14:11:42 +0100

    Tegnet vektorer og kan tilgå deres værdier

--- source/index.js
+++ source/index.js
@@ -1,32 +1,114 @@
 class Vector {
-	constructor(x_pos, y_pos, x_comp, y_comp, length, thickness, color, name) {
-		this.x = x_pos;
-		this.y = y_pos;
-		this.x_comp = x_comp;
-		this.y_comp = y_comp;
-		this.length = length;
-		this.thickness = thickness;
-		this.color = color;
-		this.name = name;
-	}
+    constructor(x_pos, y_pos, x_comp, y_comp, thickness = 3, color = 'black', name = '', arrowSize = 10) {
+        this.x = x_pos;       
+        this.y = y_pos;       
+        this.x_comp = x_comp;  
+        this.y_comp = y_comp;  
+        this.thickness = thickness;  
+        this.color = color;
+        this.name = name;
+        this.arrowSize = arrowSize; 
+    }
 
-	show() {
-		line()
-	}
+    getVector(attribute) {
+        let vector_info = [
+            'x',
+            'y',
+            'x_comp',
+            'y_comp',
+            'thickness',
+            'color',
+            'name',
+            'arrowSize'
+        ];
 
+       
+        for (let i = 0; i < vector_info.length; i++) {
+            let current = vector_info[i];
+            if (current == attribute) {
+                console.log(this[current]);
+            }
+        }
+    }
 
+    getLength() {
+        return Math.sqrt(this.x_comp ** 2 + this.y_comp ** 2);
+    }
 
+    show() {
+        stroke(this.color);     
+        strokeWeight(this.thickness); 
+        fill(this.color);        
+       
+        line(this.x, this.y, this.x + this.x_comp, this.y + this.y_comp);
+        
+        let angle = atan2(this.y_comp, this.x_comp); 
+        let endX = this.x + this.x_comp;
+        let endY = this.y + this.y_comp;
+
+        push();
+        translate(endX, endY); 
+        rotate(angle); 
+        let size = this.arrowSize; 
+        triangle(0, 0, -size, size / 2, -size, -size / 2);
+        pop();
+    }
+}
+
+function setup() {
+    let canvasWidth = 400;
+    let canvasHeight = 400;
+    createCanvas(canvasWidth, canvasHeight);
+    background(200);
+
+    let vector = new Vector(
+       	canvasWidth/2, canvasHeight/2, 
+        random(-100, 100), random(-100, 100), 
+        4, 'blue', '', 15
+    ); 
+
+    vector.show();
+
+    vector.getVector('x'); 
+    vector.getVector('color');
+}
+
+function draw() {
 }
 
 
 
 
-function setup() 
-{
-	createCanvas(400, 400);
+
+
+/* Øvelse i timen
+class A {
+    constructor() {
+        this.b = null; 
+    }
+
+    setB() {
+        this.b = new B(); 
+    }
 }
 
-function draw()
-{
+class B {
+    constructor() {
+        this.a = null; 
+    }
 
+    setA() {
+        this.a = new A(); 
+    }
 }
+
+let instanceA = new A();
+let instanceB = new B();
+
+instanceA.setB();
+instanceB.setA();
+
+console.log(instanceA);
+console.log(instanceB);
+*/ 
+
